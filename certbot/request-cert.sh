d5045c66603571f9418bb1d31066ad28e572ee22
Imesh Senanayake <115970061+imeshsen@users.noreply.github.com>
2023-10-23 09:12:42 +0530

    Update request-cert.sh

--- certbot/request-cert.sh
+++ certbot/request-cert.sh
@@ -3,4 +3,4 @@ docker run --rm -it --name certbot -v certbot-certs:/etc/letsencrypt certbot/cer
 	-m socialexpz1@gmail.com \
 	--agree-tos certonly \
 	--manual \
-	-d 'sonar.mooo.com'
+	-d 'nable.mooo.com'
